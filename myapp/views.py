489a4877e01622bf2d00411b06f2f4f63c18a030
Bishawjit-Mondol <bishawjitmondol29@gmail.com>
2025-04-20 17:02:36 +0600

    Add initial Django project structure with views, URLs, and templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Hello, world!")
+
+
+def profile(request):
+    return HttpResponse("This is the profile page.")
+
+
+def dashboard(request):
+    return HttpResponse("This is the dashboard page.")
+
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About Us'})
